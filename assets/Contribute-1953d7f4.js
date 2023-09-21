c31cc47d83528e1dde9592131f19f6c010f7c1d1
Somgonk <samuelegenkins@gmail.com>
2023-09-21 14:16:00 -0600

    distz

---
+++ assets/Contribute-1953d7f4.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-cc6509e2.js";const r={};function e(c,n){return" contribute "}const _=t(r,[["render",e]]);export{_ as default};
--- src/views/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>
-  Sorry, this page doesn't exist or has been moved to another location
-    </h1>
-  </div>
-</template>
