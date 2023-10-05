edc4faaa6daf3ee8b7a898b6864e3ab41d41e790
Somgonk <samuelegenkins@gmail.com>
2023-10-05 07:53:18 -0600

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <p>This website is open source. See the code on <a href="https://github.com/Somgonk/TabletopClub">Github</a></p>
+    <p>Made by Somgonk for Tabletop Club</p>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@import "src/variables.scss";
+
+.footer {
+  background-color: $secondary;
+  padding: 10px;
+
+}
+a {
+  all: revert;
+  color: $text;
+}
+
+</style>
